01680c399476886a690c00c4bad2e158d81ee5da
tincher <joel.ewig@stud.uni-bamberg.de>
2018-10-11 14:53:49 +0200

    v 1.0

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const csv = require('csvtojson')
-var passport = require('passport')
-var Strategy = require('passport-local').Strategy;
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
+const csv = require('csvtojson');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const schedule = require('node-schedule');
+const request = require('request');
+const util = require('util')
 
 const skucsvpath = 'C:/Users/jojoe/Desktop/projects/manne/skus.csv'
+const brandscsvpath = 'C:/Users/jojoe/Desktop/projects/manne/brands.csv'
+const categoriescsvpath = 'C:/Users/jojoe/Desktop/projects/manne/categories.csv'
+
+const bigbuyurl = 'https://api.bigbuy.eu/rest/catalog/categories.json?isoCode=de'
+
+var users = require('./users.json');
 
 
-var users = require('./users.json')
 passport.use(new Strategy(
-	function (username, password, cb) {
-		let user = JSON.parse('{"username":"' + username + '","password":"' + password + '"}')
-		users.forEach(element => {
-			if (element.username === username) {
-				if (element.password === password) {
-					return cb(null, user)
-				}
-			}
-			return cb(null, false)
-		});
-	}
+    function(username, password, cb) {
+        let user = JSON.parse('{"username":"' + username + '","password":"' + password + '"}')
+        users.forEach(element => {
+            if (element.username === username) {
+                if (element.password === password) {
+                    return cb(null, user)
+                }
+            }
+            return cb(null, false)
+        });
+    }
 ));
-passport.serializeUser(function (user, cb) {
-	cb(null, user.username);
+passport.serializeUser(function(user, cb) {
+    cb(null, user.username);
 });
-passport.deserializeUser(function (id, cb) {
-	users.forEach(element => {
-		if (element.username === id) {
-			return cb(null, element)
-		}
-	})
+passport.deserializeUser(function(id, cb) {
+    users.forEach(element => {
+        if (element.username === id) {
+            return cb(null, element)
+        }
+    })
 });
 
 const app = express();
 app.use(require('cookie-parser')());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(session({
-	secret: 'keyboard cat',
-	resave: false,
-	saveUninitialized: false
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,42 +61,157 @@ app.use(express.static('mbot-site'));
 app.listen(3000, () => console.log('MBot listening on port 3000!'));
 
 
-app.get('/test', (req, res) => {
-	res.send('success')
-})
 
-app.get('/authtest',
-	ensureLoggedIn('/'),
-	function (req, res) {
-		res.send('success2')
-	});
+// scheduled categories fetch
+
+// var j = schedule.scheduleJob('* * */2 * * *', function(){
+// var j = schedule.scheduleJob('*/20 * * * * *', function() {
+//
+//     console.log('The answer to life, the universe, and everything!');
+// });
+
+//blacklists
+
+
+app.post('/:type/delete',
+    ensureLoggedIn('/'),
+    (req, res) => {
+        let csvpath = getCsvPathForType(req.params.type)
+        item = req.body;
+        csv({
+            noheader: true
+        }).fromFile(csvpath).then((jsonObj) => {
+            let temp = jsonObj.filter(obj => (obj.field1 === item.field1))
+            jsonObj.splice(jsonObj.indexOf(temp[0]), 1);
+            const csvWriter = createCsvWriter({
+                path: csvpath,
+                header: ['field1']
+            });
+            csvWriter.writeRecords(jsonObj);
+        });
+        res.send('successfull')
+    });
+
+
+app.get('/:type/get',
+    ensureLoggedIn('/'),
+    (req, res) => {
+        if (req.params.type !== 'category') {
+            csv({
+                noheader: true
+            }).fromFile(getCsvPathForType(req.params.type)).then((jsonObj) => {
+                console.log(jsonObj);
+                res.send(jsonObj)
+            })
+        } else {
+            var categories = [];
+            var options = {
+                url: bigbuyurl,
+                headers: {
+                    'Authorization': 'Bearer ' + 'MDBlOTQyYmUyMTc3MmMwM2U2Mzk5YWYxMjdmMWI5NDdmYjE4Y2RlYTRhODFiMDUzNWRmMDBjOTQwZmYxZjNhNw'
+                }
+            }
+            request(options, (err, r, body) => {
+                if (err) {
+                    return console.log(err);
+                }
+                if (r.statusCode === 429) {
+                    res.json('Too many requests')
+                    return console.log('Too many requests')
+                }
+                var categories = JSON.parse(body).map((d) => {
+                    return {
+                        name: d.name,
+                        id: d.id,
+                        parent: d.parentCategory,
+                        blacklisted: false,
+                        children: []
+                    }
+                });
+                categories.push({
+                    name: 'root',
+                    id: 2,
+                    parent: -1,
+                    children: [],
+                    blacklisted: false
+                });
+
+                categories.forEach((category) => {
+                    category.children = categories.filter(c => c.parent === category.id).map(i => i.id);
+
+                });
+                csv({
+                    noheader: true
+                }).fromFile(getCsvPathForType(req.params.type)).then((jsonObj) => {
+                    var brandslist = jsonObj.map(brand => parseInt(brand.field1));
+                    var categoriesWithBlacklisted = categories.map(curCategory => {
+                        if (brandslist.includes(curCategory.id)) {
+                            curCategory.blacklisted = true;
+                        }
+                        return curCategory;
+                    });
+
+                    console.time('test')
+                    result = buildTree(2, categoriesWithBlacklisted);
+                    console.timeEnd('test')
+                    res.send(categoriesWithBlacklisted);
+                });
+            });
+
+
+        }
+    });
+
+app.post('/:type/add',
+    ensureLoggedIn('/'),
+    (req, res) => {
+        let csvpath = getCsvPathForType(req.params.type)
+        const csvWriter = createCsvWriter({
+            path: csvpath,
+            header: ['field1'],
+            append: true
+        });
+        csvWriter.writeRecords(req.body);
+        res.send('successfull')
+    });
+
+
+
+//authentication
 
 app.post('/login',
-	passport.authenticate('local', {
-		failureRedirect: '/'
-	}),
-	function (req, res) {
-		res.redirect('/authtest');
-	});
-
-app.get('/getskus',
-	ensureLoggedIn('/'),
-	(req, res) => {
-		csv({
-			noheader: true
-		}).fromFile(skucsvpath).then((jsonObj) => {
-			console.log(jsonObj);
-			res.send(jsonObj)
-		})
-	})
-app.post('/addsku',
-	ensureLoggedIn('/'),
-	(req, res) => {
-		console.log(req.body);
-	});
+    passport.authenticate('local', {
+        failureRedirect: '/'
+    }),
+    function(req, res) {
+        res.redirect('/blacklists/skus.html');
+    });
 
 app.get('/logout',
-	function (req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+    function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+//helper functions
+
+
+function buildTree(rootId, categoriesList) {
+    var root = categoriesList.find(x => x.id === rootId);
+    if (!root.children.length < 1) {
+        root.children = categoriesList.filter(x => root.children.includes(x.id));
+        root.children.forEach(a => buildTree(a.id, categoriesList));
+    }
+    return root;
+}
+
+function getCsvPathForType(type) {
+    if (type === 'sku') {
+        return skucsvpath;
+    } else if (type === 'brand') {
+        return brandscsvpath;
+    } else if (type === 'category') {
+        return categoriescsvpath;
+    }
+}
--- mbot-site/blacklists/blacklists.js
+++ mbot-site/blacklists/blacklists.js
@@ -1,6 +1,81 @@
 var app = new Vue({
-    el: '#skus',
+    el: '#items',
     data: {
-        test: 'Vuetest'
+        items: [],
+        newItem: '',
+        urlroot: '',
+        itemname: '',
+        ratelimitExceeded: false,
+        timer: '',
+    },
+    created: function() {
+        this.urlroot = 'http://localhost:3000/';
+        let lastUrlPart = window.location.pathname.split('/')[2];
+        console.log(lastUrlPart);
+        if (lastUrlPart === 'skus.html') {
+            this.urlroot += 'sku/';
+            this.itemname = 'SKU';
+        } else if (lastUrlPart === 'brands.html') {
+            this.urlroot += 'brand/';
+            this.itemname = 'Marke'
+        } else {
+            this.urlroot += 'category/';
+            //todo
+        }
+        this.loadItems();
+    },
+    beforeDestroy: function() {
+        clearInterval(this.timer);
+    },
+    methods: {
+        loadItems: function() {
+            this.$http.get(this.urlroot + 'get').then(response => {
+                if (response.headers.map["Content-Type"][0] === "application/json; charset=utf-8") {
+                    if (typeof response.body === "string") {
+                        this.ratelimitExceeded = true;
+                        this.timer = setInterval(this.loadItems, 150000); //todo kann entfernt werden, wenn der fetcher läuft
+                    } else {
+                        this.items = response.body;
+                        clearInterval(this.timer)
+                    }
+                } else {
+                    window.location.href = '/';
+                }
+            }, response => {
+                console.log('Error while fetching the skus');
+            })
+        },
+        deleteItem: function(item) {
+            this.$http.post(this.urlroot + 'delete', item).then(response => {
+                this.loadItems();
+            });
+            this.newItem = '';
+        },
+        addItem: function(item) {
+            let body = [{
+                field1: item
+            }]
+            this.$http.post(this.urlroot + 'add', body).then(response => {
+                this.loadItems();
+            })
+            this.newItem = '';
+        },
+        isChildOf: function(item, parentId) {
+            return item.parent === parentId;
+        },
+        filterItems: function(id) {
+            return this.items.filter(x => x.parent === id);
+        }
+
     }
 })
+
+
+$(function() {
+    $('.list-group-item').on('click', function() {
+        $('.fa', this)
+            .toggleClass('fa-caret-right')
+            .toggleClass('fa-caret-down');
+    });
+
+});
